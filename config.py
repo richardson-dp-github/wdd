e7cd59a350b8403d7534330d7102ccfb1e00a5cf
D R <richardson.dp+github@gmail.com>
2016-05-23 10:59:26 -0400

    development

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 # Test Configuration
 
+
+# Is this a test, or is this for real?
+TestMode = True
+
+
 # Central Server's Loopback Address
 centralServerIPAddress = '127.0.0.1'
 centralServerPort = 50000
@@ -12,3 +17,10 @@ initialPeriodInSeconds = 100
 #sample
 sampleFile = 'capture1-05.cap'
 
+# Rewrite Any Differences
+if TestMode == True:
+    centralServerIPAddress = '127.0.0.1'
+    centralServerPort = 50000
+else:
+    centralServerIPAddress = '127.0.0.1'
+    centralServerPort = 50000
--- slave_sniff.py
+++ slave_sniff.py
@@ -1,6 +1,6 @@
 # Slave Sniff and Send -- Test
 
-import test_config
+import config
 import scapyplist2xml
 from scapy.all import *
 # Added in order to receive data
@@ -8,31 +8,38 @@ import socket
 import sys, errno
 import time
 
-samplePeriod = test_config.initialPeriodInSeconds
+samplePeriod = config.initialPeriodInSeconds
 
 # Self
-host = test_config.slaveIPAddress
-port = test_config.slavePort
+host = config.slaveIPAddress
+port = config.slavePort
 
 # Connect to the Central Server
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-centralServerHost = test_config.centralServerIPAddress
-centralServerPort = test_config.centralServerPort
+centralServerHost = config.centralServerIPAddress
+centralServerPort = config.centralServerPort
 s.connect((centralServerHost, centralServerPort))
+
+def handle_packet(packet):
+    sendp(packet)
 
 for i in range(1,5):
     try:
         print 'starting sniff # '+str(i)+'...'
-        x = sniff(offline=test_config.sampleFile,count=5)
+        x = sniff(offline=config.sampleFile, count=15)
         print 'converting to XML...'
-        scapyplist2xml.scapyplist2xml(x, 'output_msg'+str(i)+'.xml')
+        try:
+            filename = 'output_msg'+str(i)+'.xml'
+            scapyplist2xml.scapyplist2xml(x, filename)
+            print filename + ' saved.'
+        except:
+            print "unable to convert to XML"
         f = 'output.xml'
         print 'sending message '+str(i)
-        s.send('message '+str(i)+'\n')
-    except IOError as e:
-        if e.errno == errno.EPIPE:
-            print "Error Sending Message ID "+str(i)
-
-
-
-
+        try:
+            s.send('message '+str(i)+'\n')
+        except IOError as e:
+            if e.errno == errno.EPIPE:
+                print "Error Sending Message ID "+str(i)
+    except:
+        print "Error in Sniff and Forward Operation"
