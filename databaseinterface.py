be159b15cd6dc607908d38b07ff85ba67cd62b9c
D R <richardson.dp+github@gmail.com>
2016-05-23 21:58:45 -0400

    developmental update

--- databaseinterface.py
+++ databaseinterface.py
@@ -25,17 +25,29 @@ def requestCommand():
         except:
             print 'That did not work, try again...'
 
+def insertRecord(nodeID, locTimeStamp, wifipackettype, addr1, addr2):
+    cursor = db.cursor()
+    #SQL query to INSERT a record into the table .
+    #cursor.execute('''INSERT into Packets (nodeID, locTimeStamp, wifipackettype, addr1, addr2)
+                  #values (%d, %f, %s, %s, %s)''',
+                  #(nodeID, locTimeStamp, wifipackettype, addr1, addr2))
+    cursor.execute('''INSERT into Packets (nodeID) values (1)''')  #This worked, now try the others
+    cursor.execute('''INSERT into Packets (nodeID, locTimeStamp) values (2, 1042345000.22)''')
+    # Commit your changes in the database
+    db.commit()
+
 
 # Open database connection
 db = MySQLdb.connect("localhost",userName,passWord,dbName )
 
 # append a file to the table
 ## import the file
-f = 'output_msg1.xml'
-for event, elem in ET.iterparse(f, events=('start', 'end', 'start-ns', 'end-ns')):
-   print event, elem
+#f = 'output_msg1.xml'
+#for event, elem in ET.iterparse(f, events=('start', 'end', 'start-ns', 'end-ns')):
+#   print event, elem
 
-
+# Test insertRecord
+insertRecord(1,45234442,1,'FF:FF:FF:FF:FF:FF','FF:FF:FF:FF:FF:FF')
 
 
 # disconnect from server
